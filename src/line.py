58e85eeeaa06ee6ac166a911704077057bd39181
Edward <edward.kallstedt@gmail.com>
2020-09-19 21:45:09 +0200

    hsv red masking

--- src/line.py
+++ src/line.py
@@ -5,10 +5,12 @@ from imutils.video import VideoStream
 from imutils.video import FPS
 import imutils
 
-vs = cv2.VideoCapture('red_line.mp4')
-
-
-
+vs = cv2.VideoCapture('red_cable.mp4')
+width = int(vs.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(vs.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+size = (width, height)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('your_video.avi', fourcc, 20.0, size)
 while True:
     ret, frame = vs.read()
     
@@ -19,11 +21,17 @@ while True:
         break
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # lower mask (0-10)
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(hsv, lower_red, upper_red)
 
-    lower_red = np.array([0,0,50])
-    upper_red = np.array([255,255,240])
-    
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    # upper mask (170-180)
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask = mask0 + mask1
     #output = cv2.bitwise_and(frame,frame, mask= mask)
 
     element = cv2.getStructuringElement(cv2.MORPH_CROSS,(9,9))
@@ -34,7 +42,7 @@ while True:
     #skeleton = cv2.ximgproc.thinning(eroded)
 
     edges = cv2.Canny(mask, 50, 200)
-    #edges = cv2.dilate(edges, element)
+    edges = cv2.dilate(edges, element)
     minLineLength = 100
     maxLineGap = 15
     lines = cv2.HoughLinesP(
@@ -50,12 +58,13 @@ while True:
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     # show the images
-
+    outmask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+    out.write(outmask)
     cv2.imshow('mask', mask)
     cv2.imshow('skeleton', edges)
     cv2.imshow("images", np.hstack([frame]))
 
 
-
+out.release()
 vs.release()
 cv2.destroyAllWindows()
