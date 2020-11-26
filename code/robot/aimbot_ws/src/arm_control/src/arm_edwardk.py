d139abc2fb9e1900d237d582e11e72c288d2c3ae
niklasdahlquist1 <snisse1@hotmail.com>
2020-11-26 19:11:48 +0100

    working on the arm

--- code/robot/aimbot_ws/src/arm_control/src/arm_edwardk.py
+++ code/robot/aimbot_ws/src/arm_control/src/arm_edwardk.py
@@ -6,6 +6,8 @@ from std_msgs.msg import Float64, Int32
 from sensor_msgs.msg import JointState
 from std_srvs.srv import Empty
 import time
+import pandas
+
 
 SWIVEL_MOTOR 	   = "1"
 FIRST_JOINT_MOTOR  = "2"
@@ -37,7 +39,7 @@ def convert_speeds(speed):
             dir_value = 1024
         velocity_mem_value = s/100*1023     
         vel.append(velocity_mem_value + dir_value)
-    print("speed: " + str(vel)) 
+    #print("speed: " + str(vel)) 
     return vel
 			
 class ArmControl:
@@ -48,27 +50,54 @@ class ArmControl:
     # Service to pick up a piece
     def pickup(self, req):
         # Different motor angles during pickup procedure
-        path = [[0, 0, 90, 0], [0, 0, 90, 0, 0], [0, 57, 51, 2, 0], [0, 57, 51, 2, 100], [0, 0, 90, 0, 100]]
+        file_path = "src/arm_control/src/"
+        path = pandas.read_csv(file_path + "traj_test.csv")
+        vel = pandas.read_csv(file_path + "velocity_test.csv")
+
+
+        q = []
+        velocities = []
+
+        #load positions and velocities for the trajectory
+        for i in range(0, len(path.iloc[:,0].tolist())):
+            t = path.iloc[i,:].tolist()
+            q.append(t)	
+
+        for i in range(0, len(vel.iloc[:,0].tolist())):
+            t = vel.iloc[i,:].tolist()
+            velocities.append(t)	
+            
+        path = q
+
+            #        path = [[0, 0, 90, 0], [0, 0, 90, 0, 0], [0, 57, 51, 2, 0], [0, 57, 51, 2, 100], [0, 0, 90, 0, 100]]
+            
         
-        for p in path:
+        for i in range(0, len(path)):
+            p = path[i]
+            v = velocities[i]
+            v = [abs(vv)/10 for vv in velocities[i]]
+            print("speed " + str(v))
+            print("angles " + str(p))
             joint = JointState()
             angles = p
-            print(angles)
+            #print(angles)
             joint.header.stamp = rospy.Time.now()
             joint.position = convert_angles(angles)
-            joint.velocity = convert_speeds([10, 10, 10, 10, 10])
+            #joint.velocity = convert_speeds(v)
+            joint.velocity = convert_speeds([50, 50, 50, 50, 70])
             joint.name = [SWIVEL_MOTOR, FIRST_JOINT_MOTOR, SECOND_JOINT_MOTOR, THIRD_JOINT_MOTOR, GRIPPER_MOTOR]
             self.arm_publisher.publish(joint)
-            time.sleep(3)
+            time.sleep(0.05)
         return ()
-	        
-	# Service to return to line follow position (does not touch gripper since it might be in use)
+
+    # Service to return to line follow position (does not touch gripper since it might be in use)
     def reset_arm(self, req):
         joint = JointState()
         joint.header.stamp = rospy.Time.now()
         joint.position = convert_angles([0, 0, 90, 0])
         joint.velocity = convert_speeds([10, 10, 10, 10])
         joint.name = [SWIVEL_MOTOR, FIRST_JOINT_MOTOR, SECOND_JOINT_MOTOR, THIRD_JOINT_MOTOR]
+        rospy.logwarn("In arm reset")
         self.arm_publisher.publish(joint)
         return ()
 
