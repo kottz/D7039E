89c6b87d0d4891a5df7a86e6ea9bd7f6a7c29e50
AlbinMartinsson <albmar-6@student.ltu.se>
2020-11-30 15:32:28 +0100

    Last minute fixes for running on robot.

--- code/arrowhead/consumer.py
+++ code/arrowhead/consumer.py
@@ -9,19 +9,36 @@ with open("config_template.json") as json_file:
     config = json.load(json_file)[0]
     consumer_json = config["consumer_json"]
     consumer_name = config["consumer_json"]["systemName"]
+    provider_json = config["provider_json"]
     provider_name = config["provider_json"]["systemName"]
+    pick_up_service_json = config["pick_up_service_json"]
+    pick_up_service_definition = config["pick_up_service_json"]["serviceDefinition"]
+    place_service_json = config["place_service_json"]
+    place_service_definition = config["place_service_json"]["serviceDefinition"]
+
+test_provider = Arrowhead_system(
+    "/home/albin/Documents/core-java-spring/certificates/testcloud2/sysop.p12", "123456")
+
+test_provider.register_system(provider_json)
+test_provider.register_service(pick_up_service_json)
+test_provider.add_intracloud_authorization(pick_up_service_definition).json()
+test_provider.create_orchestration_store_entry(pick_up_service_definition)
+test_provider.add_intracloud_authorization(place_service_definition)
+test_provider.create_orchestration_store_entry(place_service_definition)
+
 
 test_consumer = Arrowhead_system(
     "/home/albin/Documents/core-java-spring/certificates/testcloud2/sysop.p12", "123456")
 test_consumer.register_system(consumer_json)
 test_consumer.start_orchestration_based_on_id(consumer_name, provider_name)
 
+# TODO flask api
+json = {
+    'ready': True
+}
 
-#TODO flask api
-# json = {
-#     'ready': True
-# }
+print(post('http://94.255.133.57:2342/pick-up',
+           verify=False, json=json
+           ).json())
 
-# print(post('http://localhost:5000/Ready-for-pickup',
-#            verify=False, json=json
-#            ).json())
+print(get('http://94.255.133.57:2342/place'))
--- code/arrowhead/provider.py
+++ code/arrowhead/provider.py
@@ -1,36 +1,6 @@
 import json
 from flask import Flask, jsonify, request
 from requests_pkcs12 import get, post
-from arrowhead_core_systems import Arrowhead_system
-
-
-
-# TODO add code from core_systems.py to take care of all arrowhead logic.
-with open("config_template.json") as json_file:
-    config = json.load(json_file)[0]
-    consumer_json = config["consumer_json"]
-    consumer_name = config["consumer_json"]["systemName"]
-    provider_json = config["provider_json"]
-    provider_name = config["provider_json"]["systemName"]
-    pick_up_service_json = config["pick_up_service_json"]
-    pick_up_service_definition = config["pick_up_service_json"]["serviceDefinition"]
-    place_service_json = config["place_service_json"]
-    place_service_definition = config["place_service_json"]["serviceDefinition"]
-
-test_provider = Arrowhead_system(
-    "/home/albin/Documents/core-java-spring/certificates/testcloud2/sysop.p12", "123456")
-
-test_provider.register_system(provider_json)
-test_provider.register_service(pick_up_service_json)
-print(test_provider.register_service(place_service_json).json())
-test_provider.add_intracloud_authorization(pick_up_service_definition).json()
-print(test_provider.create_orchestration_store_entry(pick_up_service_definition))
-test_provider.add_intracloud_authorization(place_service_definition).json()
-print(test_provider.create_orchestration_store_entry(place_service_definition))
-print(test_provider.start_orchestration_based_on_id(consumer_name, provider_name).json())
-
-
-
 
 # creating a Flask app
 app = Flask(__name__)
@@ -50,14 +20,18 @@ def disp(num):
     return jsonify({'data': num**2})
 
 
-@app.route('/Ready-for-pickup', methods=['POST'])
-def service1():
+@app.route('/pick-up', methods=['POST'])
+def ready_for_pick_up():
     info = {
         'ready': request.json['ready']
     }
 
     return jsonify({'info': info}), 201
 
-
+@app.route('/place', methods=['GET'])
+def ready_for_place():
+        data = "hello world"
+        return jsonify({'data': data})
+ 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="2342")
