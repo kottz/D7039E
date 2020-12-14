a9c6cf2549586ef37c68d44c64aef0d489036796
AlbinMartinsson <albmar-6@student.ltu.se>
2020-12-14 17:14:06 +0100

    Added "goal" to consumer

--- code/robot/aimbot_ws/src/arrowhead/src/ros_arrowhead.py
+++ code/robot/aimbot_ws/src/arrowhead/src/ros_arrowhead.py
@@ -2,6 +2,43 @@
 import rospy
 import threading
 from arm_control.srv import *
+import json
+import time
+from flask import Flask, jsonify, request
+from arrowhead_core_systems import Arrowhead_system
+from requests_pkcs12 import get, post
+
+# Fix path to file (config_template_json)
+with open("config_template.json") as json_file:
+    config = json.load(json_file)[0]
+    consumer_json = config["consumer_json"]
+    consumer_name = config["consumer_json"]["systemName"]
+    provider_json = config["provider_json"]
+    provider_name = config["provider_json"]["systemName"]
+
+# Fix path to file (sysop.p12)
+test_provider = Arrowhead_system(
+    "/home/albin/Documents/core-java-spring/certificates/testcloud2/sysop.p12", "123456")
+
+test_provider.register_system(provider_json)
+
+class Arrowhead_response:
+    def __init__(self):
+        self.direction = ""
+        self.ready = "not ready"
+
+ah_resp = Arrowhead_response()
+
+# creating a Flask app
+app = Flask(__name__)
+
+@app.route('/pick-up', methods=['POST'])
+def ready_for_pick_up():
+    ah_resp.ready = request.json['ready']
+    ready = {
+        'ready': request.json['ready']
+    }
+    return jsonify({'ready': ready}), 201
 
 def get_keyboard_input():
       while(True):
@@ -23,6 +60,13 @@ def get_keyboard_input():
             return
         self.n = int(inp)
 
+#Clean this up
+def set_goal(request):
+    if ah_resp.ready == "ready":
+        return "goal 1:1"
+    else:
+        return ""
+
 def arrowhead_spoof(request):
     qr_data = request
     print(qr_data)
@@ -30,6 +74,7 @@ def arrowhead_spoof(request):
     # sry for ugly
     print("key: " + inp)
     return inp
+
     
 
 
@@ -39,5 +84,7 @@ if __name__ == '__main__':
     #x = threading.Thread(target=get_keyboard_input)
     #x.start()
     #x.join()
-    rospy.Service('ah_req', ah_request, arrowhead_spoof)
+    #rospy.Service('ah_req', ah_request, arrowhead_spoof)
+    rospy.Service('ah_req', ah_request, set_goal)
+    app.run(host="0.0.0.0", port="2342", debug=False)
     rospy.spin()
